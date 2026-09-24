a53cb35ca5b157b49891fe903cf8f5a6cd2b9932
Justin Valle <jvall6@uic.edu>
2026-09-24 12:46:41 -0500

    step 7a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -3,3 +3,14 @@ library(usethis)
 git_default_branch()
 
 # Default Branch Name: main
+# Step 7
+library(tidyverse)
+library(readr)
+library(ggplot2)
+olympics <- read.csv("Olympics.csv")
+# 7a
+delegation_1992 <- olympics %>%
+  filter(year == 1992) %>%
+  select(country, athletes) %>%
+  arrange(desc(athletes))
+delegation_1992
